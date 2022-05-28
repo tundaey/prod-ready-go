55e3489eb9d1d586836fb2c426f5e4060043be5e
Tunde Ganiy <tundaey@gmail.com>
2022-05-28 09:18:19 +0100

    structuring our app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up our API")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go REST API")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println((err))
+	}
 }
